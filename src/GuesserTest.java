d17efb44667c77bd24fc8726d3a1d0b14feb0a38
Öyvind Borgersen <oybo84@gmail.com>
2018-12-02 13:14:22 +0000

    Testing handling of nullpointerexception

--- src/GuesserTest.java
+++ src/GuesserTest.java
@@ -2,8 +2,18 @@ public class GuesserTest {
 
     public static void main(String[] args) {
         String s = null;
-        if(s.equals("Barcelona")){
+
+        if("Barcelona".equals(s)){
             System.out.println("Ay, caramba");
         }
+
+        try{
+            if (s.equals("Barcelona")){
+                System.out.println("Ay, caramba");
+            }
+        }catch (NullPointerException npe){
+            System.out.println("This did not work");
+        }
+
     }
 }
