387528435d0286963a5f3fa913422532523f91de
Öyvind Borgersen <oybo84@gmail.com>
2018-12-02 13:07:07 +0000

    Main class modified to let player choose interval

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -2,7 +2,10 @@ public class GuessingGame {
 
     public static void main(String[] args) {
 
-        Guesser a = new Guesser(0,1000);
+        int low = Integer.parseInt(args[0]);
+        int high = Integer.parseInt(args[1]);
+
+        Guesser a = new Guesser(low,high);
         a.start();
     }
 }
