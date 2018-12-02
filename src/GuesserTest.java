968947b6712db4fc0e76249a26ce752802cd93d9
Öyvind Borgersen <oybo84@gmail.com>
2018-12-02 11:12:17 +0000

    Test class added

---
+++ src/GuesserTest.java
@@ -0,0 +1,9 @@
+public class GuesserTest {
+
+    public static void main(String[] args) {
+        String s = null;
+        if(s.equals("Barcelona")){
+            System.out.println("Ay, caramba");
+        }
+    }
+}
