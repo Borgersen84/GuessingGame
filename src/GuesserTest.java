c28c9180d5009c099ff874643c37a11b4f4a352b
Öyvind <oybo84@gmail.com>
2018-12-18 12:52:31 +0100

    Expanded the constructor test

--- src/GuesserTest.java
+++ src/GuesserTest.java
@@ -2,14 +2,36 @@ public class GuesserTest {
 
     public static void main(String[] args) {
 
+        Guesser guesser;
         //Testing the constructor for exception
         try {
-            Guesser guesser = new Guesser(1000, 10);
+            guesser = new Guesser(1000, 10);
             System.err.println("Test failed. No exception thrown");
         }catch (IllegalArgumentException iae){
             System.err.println(iae);
-            System.out.println("Test lyckades!");
+            System.out.println("Test succeeded!");
         }
+
+        // Testing the constructor with equal value on both parameters
+        try {
+            guesser = new Guesser(100,100);
+            System.err.println("Test failed. No exception thrown");
+        }catch (IllegalArgumentException iae){
+            System.err.println(iae);
+            System.out.println("Test succeeded!");
+        }
+
+        // Testing the constructor with correct data
+        try {
+            guesser = new Guesser(1,100);
+            System.out.println("This is correct!");
+        }catch (IllegalArgumentException iae){
+            System.err.println("Test failed. Constructor is not working properly");
+        }
+
+
+
+
 
     }
 }
